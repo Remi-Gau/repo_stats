2ad43b5bdd9c66f39dcc75b98d41ec44881005c2
Remi Gau <remi_gau@hotmail.com>
2023-07-05 19:09:47 +0200

    time to fix

--- bug_fixed_issues.py
+++ bug_fixed_issues.py
@@ -14,6 +14,7 @@ USERNAME = "Remi-Gau"
 
 # may require a token if run often
 TOKEN_FILE = Path(__file__).parent / ("token.txt")
+TOKEN_FILE = Path("/home/remi/Documents/tokens/gh_read_repo.txt")
 
 # repo to check
 GH_USERNAME = "nilearn"
@@ -22,33 +23,10 @@ GH_REPO = "nilearn"
 DEBUG = False
 
 exclude_users = [
-    "dependabot[bot]",
-    "pre-commit-ci[bot]",
-    "allcontributors[bot]",
-    "github-actions[bot]",
 ]
-
-CORE_DEVS = [
-    "GaelVaroquaux",
-    "alexisthual",
-    "bthirion",
-    "emdupre",
-    "htwangtw",
-    "jeromedockes",
-    "Nicolas Gensollen",
-    "Remi-Gau",
-    "tsalo",
-    "ymzayek",
-    "AlexandreAbraham",
-    "KamalakerDadi",
-    "lesteve",
-    "pgervais",
-    "kchawla-pi",
-]
-
 
 # Set to true to rely on presaved list of PRs or issues
-USE_LOCAL = True
+USE_LOCAL = False
 
 
 def root_folder():
@@ -56,7 +34,7 @@ def root_folder():
     return Path(__file__).parent
 
 
-def get_list_of_closed_prs(gh_username: str, gh_repo: str, auth=None):
+def get_list_of_closed_issues(gh_username: str, gh_repo: str, auth=None):
     """List open PRs for a given repo.
 
     Parameters
@@ -75,10 +53,10 @@ def get_list_of_closed_prs(gh_username: str, gh_repo: str, auth=None):
     """
     closed_prs = []
     base_url = "https://api.github.com/repos/"
-    url = f"{base_url}{gh_username}/{gh_repo}/pulls?per_page=100&state=closed&page="
-    for page in range(1, 100):
+    url = f"{base_url}{gh_username}/{gh_repo}/issues?per_page=100&state=closed&page="
+    for page in range(1, 200):
         url_page = f"{url}{page}"
-        print(f"Getting page {page} of PRs")
+        print(f"Getting page {page} of issues")
         response = requests.get(f"{url_page}", auth=auth)
         if DEBUG and page > 1:
             break
@@ -92,81 +70,64 @@ def get_list_of_closed_prs(gh_username: str, gh_repo: str, auth=None):
 def main():
     """Get PRs, save their diffs to files and list all files touched by PRs."""
 
-    output_file = root_folder() / f"closed_prs_{GH_USERNAME}_{GH_REPO}"
+    output_file = root_folder() / f"closed_issues_{GH_USERNAME}_{GH_REPO}"
 
     TOKEN = None
     if TOKEN_FILE.exists():
-        with open(Path(__file__).parent / "token.txt") as f:
+        with open(TOKEN_FILE) as f:
             TOKEN = f.read().strip()
 
     auth = None if USERNAME is None or TOKEN is None else (USERNAME, TOKEN)
 
     if not USE_LOCAL:
-        pulls = get_list_of_closed_prs(
+        issues = get_list_of_closed_issues(
             gh_username=GH_USERNAME, gh_repo=GH_REPO, auth=auth
         )
         with open(output_file.with_suffix(".json"), "w") as f:
-            json.dump(pulls, f, indent=2)
+            json.dump(issues, f, indent=2)
     else:
         with open(output_file.with_suffix(".json")) as f:
-            pulls = json.load(f)
+            issues = json.load(f)
 
     data = {
         "number": [],
         "created_at": [],
-        "merged_at": [],
+        "closed_at": [],
         "state": [],
         "title": [],
         "user": [],
     }
-    for pr in pulls:
-        if pr["merged_at"] and pr["user"]["login"] not in exclude_users:
-            data["number"].append(pr["number"])
-            data["created_at"].append(pr["created_at"])
-            data["merged_at"].append(pr["merged_at"])
-            data["state"].append(pr["state"])
-            data["title"].append(pr["title"])
-            data["user"].append(pr["user"]["login"])
+    for issue_ in issues:
+        labels = [x["name"] for x in issue_["labels"]]
+        if issue_["closed_at"] and 'Bug' in labels:
+            data["number"].append(issue_["number"])
+            data["created_at"].append(issue_["created_at"])
+            data["closed_at"].append(issue_["closed_at"])
+            data["state"].append(issue_["state"])
+            data["title"].append(issue_["title"])
+            data["user"].append(issue_["user"]["login"])    
 
     df = pd.DataFrame(data)
 
     df["created_at"] = pd.to_datetime(df["created_at"])
-    df["merged_at"] = pd.to_datetime(df["merged_at"])
-    df["time_to_merge"] = (df["merged_at"] - df["created_at"]).dt.days
+    df["closed_at"] = pd.to_datetime(df["closed_at"])
+    df["time_to_fix"] = (df["closed_at"] - df["created_at"]).dt.days
 
-    # threshold time to merge to X days
-    df.loc[df["time_to_merge"] >= 90, "time_to_merge"] = 90
+    # threshold time_to_fix to X days
+    df.loc[df["time_to_fix"] >= 180, "time_to_fix"] = 180    
 
     df.to_csv(output_file.with_suffix(".tsv"), index=False, sep="\t")
 
     # use plotly to plot histogram of time to merge on a logaithmic scale
     fig = px.histogram(
         df,
-        x="time_to_merge",
+        x="time_to_fix",
         nbins=90,
-        range_x=[0, 90],
-        title="Time to merge PRs (days)",
+        range_x=[0, 180],
+        title="Time to fix bug (days)",
     )
     fig.write_html(output_file.with_suffix(".html"))
-    fig.show()
-
-    # remove PRs from core devs
-    output_file = root_folder() / f"closed_prs_{GH_USERNAME}_{GH_REPO}_noCoreDev"
-    df = df[~df["user"].isin(CORE_DEVS)]
-    df.to_csv(output_file.with_suffix(".tsv"), index=False, sep="\t")
-    fig = px.histogram(
-        df,
-        x="time_to_merge",
-        nbins=90,
-        range_x=[0, 90],
-        title="Time to merge PRs (days) - no core devs",
-    )
-    fig.write_html(output_file.with_suffix(".html"))
-    fig.show()
-
-    #  show unique users
-    print(sorted(df["user"].unique()))
-
+    fig.show()    
 
 if __name__ == "__main__":
     main()
